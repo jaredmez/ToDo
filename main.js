6e3e2c0ddd8cbcbb960f5d85a81018d0429a720b
Jared Mezquita <mezquitaj@gmail.com>
2021-08-26 15:17:57 -0400

    Add Project input popup

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-//constructor for a ToDo item
 
+//Project Task Control
+
+function onload () {
+    document.querySelector(".add-project-btn").addEventListener("click", addNewProject);
+}
+
+
+
+function addNewProject() {
+
+    showProjectPopup();
+
+    console.log('added project !')
+
+
+
+    //need to create a new list element
+    //show input for user to enter project title
+    //have a submit button/cancel button along with input
+    //on submit generate the project item 
+    //do I call the object? whats in the object? 
+ 
+}
+
+const showProjectPopup = () => {
+    document.querySelector('.project-input-popup').style.display = "block"
+};
+
+
+
+//constructor for a ToDo item
 class todoItem {
     constructor(title, description, dueDate, priority) {
         this.title = title;
@@ -9,10 +39,10 @@ class todoItem {
     }
 }
 
-itemOne = new todoItem('Read OOP Article', 'Get acquainted with solid principles', 'However Long', 'Really Important');
-console.log(itemOne);
+class projectTask {
+    constructor(title) {
+        this.title = title;
+    }
+}
 
-//show todoItem
-
-document.body.style.background = "pink";
-setTimeout(() => document.body.style.background = "",5000);
+onload();
